cc7eed1c7dc6581b24a2936acc777594f7cc7758
alicjamusial <alicja@musial.pro>
2019-10-08 12:00:57 +0200

    Audio & waterfall URLs

--- satnogs_crawler/satnogs_crawler/items.py
+++ satnogs_crawler/satnogs_crawler/items.py
@@ -6,5 +6,8 @@ class SatnogsCrawlerItem(scrapy.Item):
     id = scrapy.Field()
     startDate = scrapy.Field()
     endDate = scrapy.Field()
+    audioUrl = scrapy.Field()
+    waterfallUrl = scrapy.Field()
     url = scrapy.Field()
+
     pass
--- satnogs_crawler/satnogs_crawler/spiders/satnogs_spider.py
+++ satnogs_crawler/satnogs_crawler/spiders/satnogs_spider.py
@@ -3,6 +3,8 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from satnogs_crawler.items import SatnogsCrawlerItem
 
+def add_url_base(url):
+    return "https://network.satnogs.org" + url
 
 class SatnogsSpider(CrawlSpider):
     name = "satnogs"
@@ -20,16 +22,15 @@ class SatnogsSpider(CrawlSpider):
     def parse_start_url(self, response):
         item_links = response.css('.obs-link::attr(href)').extract()
         for link in item_links:
-            yield scrapy.Request("https://network.satnogs.org" + link, callback=self.parse_observation_page)
+            yield scrapy.Request(add_url_base(link), callback=self.parse_observation_page)
 
     def parse_observation_page(self, response):
         observationId = response.css('#observation-info::text').extract()[0].strip()
         startDate = response.css('.datetime-data .datetime-date::text').extract()[0] + " " + response.css('.datetime-data .datetime-time::text').extract()[0]
         endDate = response.css('.datetime-data .datetime-date::text').extract()[1] + " " + response.css('.datetime-data .datetime-time::text').extract()[1]
 
-        item = SatnogsCrawlerItem()
-        item['id'] = observationId
-        item['startDate'] = startDate
-        item['endDate'] = endDate
-        item['url'] = response.url
+        audioUrl = add_url_base(response.css('.front-line:last-child .front-data a::attr(href)').extract()[0])
+        waterfallUrl = add_url_base(response.css('.front-line:last-child .front-data a::attr(href)').extract()[1])
+
+        item = SatnogsCrawlerItem(id=observationId, startDate=startDate, endDate=endDate, audioUrl=audioUrl, waterfallUrl=waterfallUrl, url=response.url)
         yield item
